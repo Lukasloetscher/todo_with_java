51248ca46893b71a54a6e06f013e339ccab97818
Lukas Lötscher <lukas@loetscher.swiss>
2025-03-31 21:07:49 +0200

    added comment to class

--- todo_with_java/src/myutility/MyLogger.java
+++ todo_with_java/src/myutility/MyLogger.java
@@ -1,5 +1,7 @@
 package myutility;
 
+
+//the purpose of this is that, i can easily later condense all my loggs to a specific place without needing to search my println statements one by one.
 public  class MyLogger {
 	
 	public static void println(String text) {
